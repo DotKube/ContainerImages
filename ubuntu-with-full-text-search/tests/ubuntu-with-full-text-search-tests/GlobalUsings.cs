109e8d13e7ee9eed5313a189ad69ea1bd19a9e7b
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2025-01-19 14:04:05 -0500

    adding tests for the container being built

---
+++ ubuntu-with-full-text-search/tests/ubuntu-with-full-text-search-tests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using Xunit;
+global using System.Text.Json;
+global using Docker.DotNet;
+global using Docker.DotNet.Models;
---
+++ ubuntu-with-full-text-search/tests/ubuntu-with-full-text-search-tests/SqlServerContainerTests.cs
@@ -0,0 +1,280 @@
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+
+namespace SqlServerTests
+{
+    public class SqlServerContainerTests : IAsyncLifetime
+    {
+        private DockerClient _dockerClient;
+        private const string RegistryContainerName = "docker-registry";
+        private const string RegistryImage = "registry:2";
+        private const int RegistryPort = 5001;
+        private const string ImageName = "localhost:5001/sql-server-full-text-search:latest";
+        private const string ContainerName = "mssql-server-test";
+        private const string SaPassword = "YourStrong!Password";
+        private readonly string _connectionString = "Server=127.0.0.1,1433;Database=master;User Id=sa;Password=YourStrong!Password;TrustServerCertificate=true;Encrypt=false";
+
+        public async Task InitializeAsync()
+        {
+            _dockerClient = CreateDockerClient();
+
+            // Start the Docker registry
+            await StartRegistryAsync();
+
+            // Build the image
+            await BuildImageAsync();
+
+            // Remove any existing container with the same name
+            var containers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters { });
+            foreach (var container in containers.Where(c => c.Names.Any(name => name.Contains(ContainerName))))
+            {
+                await _dockerClient.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters { Force = true });
+            }
+
+            // Create and start the SQL Server container
+            await _dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Name = ContainerName,
+                Image = ImageName,
+                Env = new List<string>
+                {
+                    "ACCEPT_EULA=Y",
+                    $"MSSQL_SA_PASSWORD={SaPassword}",
+                    "MSSQL_AGENT_ENABLED=True"
+                },
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        { "1433/tcp", new List<PortBinding> { new PortBinding { HostPort = "1433" } } }
+                    }
+                }
+            });
+
+            await _dockerClient.Containers.StartContainerAsync(ContainerName, new ContainerStartParameters());
+
+            // Wait for SQL Server to start
+            await Task.Delay(TimeSpan.FromSeconds(20));
+        }
+
+        public async Task DisposeAsync()
+        {
+            // Stop and remove the SQL Server container
+            var containers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
+            foreach (var container in containers.Where(c => c.Names.Contains($"/{ContainerName}")))
+            {
+                await _dockerClient.Containers.StopContainerAsync(container.ID, new ContainerStopParameters { WaitBeforeKillSeconds = 10 });
+                await _dockerClient.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters { Force = true });
+            }
+
+            // Stop and remove the Docker registry container
+            var registryContainers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
+            foreach (var container in registryContainers.Where(c => c.Names.Contains($"/{RegistryContainerName}")))
+            {
+                await _dockerClient.Containers.StopContainerAsync(container.ID, new ContainerStopParameters { WaitBeforeKillSeconds = 10 });
+                await _dockerClient.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters { Force = true });
+            }
+
+            _dockerClient.Dispose();
+        }
+
+        private async Task StartRegistryAsync()
+        {
+            // Remove any existing registry container
+            var containers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
+            var existingRegistry = containers.FirstOrDefault(c => c.Names.Contains($"/{RegistryContainerName}"));
+            if (existingRegistry != null)
+            {
+                await _dockerClient.Containers.RemoveContainerAsync(existingRegistry.ID, new ContainerRemoveParameters { Force = true });
+            }
+
+            // Create and start the registry container
+            await _dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Name = RegistryContainerName,
+                Image = RegistryImage,
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        { "5000/tcp", new List<PortBinding> { new PortBinding { HostPort = RegistryPort.ToString() } } }
+                    }
+                }
+            });
+
+            await _dockerClient.Containers.StartContainerAsync(RegistryContainerName, new ContainerStartParameters());
+        }
+
+        private async Task BuildImageAsync()
+        {
+            var buildContextPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..");
+            var tarballPath = Path.Combine(Path.GetTempPath(), "docker-build-context.tar");
+
+            CreateTarball(buildContextPath, tarballPath);
+
+            using (var tarStream = new MemoryStream(File.ReadAllBytes(tarballPath)))
+            {
+                var buildParameters = new ImageBuildParameters
+                {
+                    Dockerfile = "Dockerfile",
+                    Tags = new List<string> { ImageName }
+                };
+
+                var progress = new Progress<JSONMessage>(message =>
+                {
+                    if (!string.IsNullOrWhiteSpace(message.Stream))
+                    {
+                        Console.WriteLine(message.Stream.Trim());
+                    }
+                });
+
+                tarStream.Seek(0, SeekOrigin.Begin);
+
+                await _dockerClient.Images.BuildImageFromDockerfileAsync(
+                    buildParameters,
+                    tarStream,
+                    null,
+                    new Dictionary<string, string>(),
+                    progress,
+                    CancellationToken.None
+                );
+            }
+
+            if (File.Exists(tarballPath))
+            {
+                File.Delete(tarballPath);
+            }
+        }
+
+        private static void CreateTarball(string sourceDirectory, string tarballPath)
+        {
+            using (var tarStream = File.Create(tarballPath))
+            using (var archive = new SharpCompress.Writers.Tar.TarWriter(tarStream, new SharpCompress.Writers.Tar.TarWriterOptions(SharpCompress.Common.CompressionType.None, true)))
+            {
+                foreach (var filePath in Directory.GetFiles(sourceDirectory, "*", SearchOption.AllDirectories))
+                {
+                    var entryPath = Path.GetRelativePath(sourceDirectory, filePath).Replace("\\", "/");
+                    using var fileStream = File.OpenRead(filePath);
+                    archive.Write(entryPath, fileStream, DateTime.UtcNow);
+                }
+            }
+        }
+
+        private DockerClient CreateDockerClient()
+        {
+            var config = new DockerClientConfiguration(new Uri(GetClientUri()));
+            return config.CreateClient();
+
+            string GetClientUri()
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return "npipe://./pipe/docker_engine";
+                }
+                else
+                {
+                    string podmanPath = $"/run/user/{geteuid()}/podman/podman.sock";
+                    if (File.Exists(podmanPath))
+                    {
+                        return $"unix:{podmanPath}";
+                    }
+
+                    return "unix:/var/run/docker.sock";
+                }
+            }
+
+            [DllImport("libc")]
+            static extern uint geteuid();
+        }
+
+        [Fact]
+        public void Test_SqlServerConnection_ShouldConnectSuccessfully()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                Assert.True(connection.State == System.Data.ConnectionState.Open, "SQL Server connection should be open.");
+            }
+        }
+
+        [Fact]
+        public void Test_FullTextSearchInstallation_ShouldBeInstalled()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "SELECT SERVERPROPERTY('IsFullTextInstalled') AS IsFullTextInstalled;";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    var result = command.ExecuteScalar();
+                    Assert.NotNull(result);
+                    Assert.Equal(1, Convert.ToInt32(result)); // FTS should be installed and return 1
+                }
+            }
+        }
+
+        [Fact]
+        public void Test_CreateFullTextCatalog_ShouldSucceed()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string createDbQuery = "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'TestFTS') CREATE DATABASE TestFTS;";
+                using (SqlCommand command = new SqlCommand(createDbQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                connection.ChangeDatabase("TestFTS");
+
+                string createTableQuery = @"
+                    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'TestTable' AND xtype = 'U')
+                    CREATE TABLE TestTable (
+                        Id INT NOT NULL,
+                        TextContent NVARCHAR(MAX),
+                        CONSTRAINT PK_TestTable PRIMARY KEY (Id)
+                    );
+
+                    IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_TestTable_Id')
+                    CREATE UNIQUE INDEX IX_TestTable_Id ON TestTable(Id);
+                ";
+
+                using (SqlCommand command = new SqlCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string createFTSCatalogQuery = @"
+                        IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'TestFTS_Catalog')
+                        BEGIN
+                            CREATE FULLTEXT CATALOG TestFTS_Catalog AS DEFAULT;
+                            CREATE FULLTEXT INDEX ON TestTable (TextContent) KEY INDEX PK_TestTable;
+                        END";
+                using (SqlCommand command = new SqlCommand(createFTSCatalogQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string verifyFTSCatalogQuery = "SELECT COUNT(*) FROM sys.fulltext_catalogs WHERE name = 'TestFTS_Catalog';";
+                using (SqlCommand command = new SqlCommand(verifyFTSCatalogQuery, connection))
+                {
+                    var result = command.ExecuteScalar();
+                    Assert.NotNull(result);
+                    Assert.Equal(1, Convert.ToInt32(result));
+                }
+            }
+        }
+    }
+}
